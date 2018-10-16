cb615fd04b38459dc849df8a2ae221b12149d5e3
Matt <MVSucharski@gmail.com>
2018-10-15 22:14:59 -0400

    almost done

---
+++ main.js
@@ -0,0 +1,37 @@
+let ant = document.querySelector('#ant');
+let pat = document.querySelector('#pat');
+let modal = document.querySelector('.modal');
+let modalPatt = document.querySelector('.modalPatt');
+let closeBtn = document.querySelector('.closeBtn');
+let closeBtnPatt = document.querySelector('#closeBtnPatt');
+
+
+
+  function openModal() {
+    modal.style.display = 'block';
+  };
+
+  function openPat() {
+    modalPatt.style.display = 'block';
+  };
+
+  function closeModal() {
+    modal.style.display = 'none';
+      };
+
+  function closePatt() {
+    modalPatt.style.display = 'none';
+      };
+  
+  function outsideClick(e) {
+    if(e.target == modal || e.target == modalPatt) {
+    modal.style.display = 'none';
+    modalPatt.style.display = 'none';
+    }
+  };
+
+  ant.addEventListener('click', openModal);
+  pat.addEventListener('click', openPat);
+  closeBtn.addEventListener('click', closeModal);
+  closeBtnPatt.addEventListener('click', closePatt);
+  window.addEventListener('click', outsideClick);
